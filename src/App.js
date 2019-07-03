3bf2110576f275b399f7fcc8ce1e53ca558688e3
Dagestan05 <steklofan@gmail.com>
2019-07-03 22:57:51 +0200

    added navbar using MaterialUI, styles with cssinJS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import RegisterForm from './RegisterForm';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
+      <RegisterForm/>
     </div>
   );
 }
---
+++ src/RegisterForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RegisterForm extends Component {
+  render() {
+    return (
+      <div>
+        Register
+      </div>
+    );
+  }
+}
+
+export default RegisterForm;
