8684809b75e5e4e01ce49f0fd2756734256f295f
Dagestan05 <steklofan@gmail.com>
2019-07-05 23:25:03 +0200

    refactored class comnts to func compnts and used useXontext hooks

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import styles from './styles/NavbarStyles';
 //Material UI imports
 import AppBar from '@material-ui/core/AppBar';
@@ -10,7 +10,7 @@ import SearchIcon from '@material-ui/icons/Search';
 import Switch from '@material-ui/core/Switch';
 import { withStyles } from '@material-ui/core/styles'
 import { ThemeContext } from './contexts/ThemeContext';
-import withLanguageContext from './hocs/withLanguageContext';
+import { LanguageContext } from './contexts/LanguageContext';
 
 const localeVars = {
   english: {
@@ -26,45 +26,40 @@ const localeVars = {
     flag: '🇪🇸'
   },
 }
-class Navbar extends Component {
-  //context Type, looks for context up in the compnt tree,
-  //and makes it accessible with this.context,
-  //this.context will be obj
-  static contextType = ThemeContext;
+function Navbar(props) {
+  
+  const { isDarkMode, toggleTheme } = useContext(ThemeContext);
+  const {classes} = props;
+  const {language} = useContext(LanguageContext);
+  const { search, flag } = localeVars[language]
 
-  render() {
-    const { isDarkMode, toggleTheme } = this.context;
-    const {classes} = this.props;
-    const {language} = this.props.languageCTX;
-    const { search, flag } = localeVars[language]
-    return (
-      <div className={classes.root}>
-        <AppBar position="static" color={isDarkMode ? "default": "primary"}>
-          <Toolbar>
-            <IconButton className={classes.menuButton} color="inherit">
-              <span>
-                {flag}
-              </span>
-            </IconButton>
-            <Typography className={classes.title} variant="h6" color="inherit">
-              App Title
-            </Typography>
-            <Switch onChange={toggleTheme}/>
-            <div className={classes.grow}/>
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon/>
-              </div>
-              <InputBase placeholder={`${search}...`} classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}/>
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color={isDarkMode ? "default": "primary"}>
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit">
+            <span>
+              {flag}
+            </span>
+          </IconButton>
+          <Typography className={classes.title} variant="h6" color="inherit">
+            App Title
+          </Typography>
+          <Switch onChange={toggleTheme}/>
+          <div className={classes.grow}/>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon/>
             </div>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-  }
+            <InputBase placeholder={`${search}...`} classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}/>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
-export default withLanguageContext(withStyles(styles)(Navbar));
+export default withStyles(styles)(Navbar);
--- src/RegisterForm.js
+++ src/RegisterForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {useContext} from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import FormControl from "@material-ui/core/FormControl";
@@ -35,51 +35,50 @@ const words = {
     remember: "Recuérdame"
   }
 };
-class Form extends Component {
-  static contextType= LanguageContext;
 
-  render() {
-    const { classes } = this.props;
-    const {language, toggleLanguage} = this.context;
-    const { email, signIn, password, remember } = words[language];
-    return (
-      <main className={classes.main}>
-        <Paper className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography variant='h5'>{signIn}</Typography>
-          <Select onChange={toggleLanguage} value={language}>
-            <MenuItem value='english'>English</MenuItem>
-            <MenuItem value='french'>French</MenuItem>
-            <MenuItem value='spanish'>Spanish</MenuItem>
-          </Select>
-          <form className={classes.form}>
-            <FormControl margin='normal' required fullWidth>
-              <InputLabel htmlFor='email'>{email}</InputLabel>
-              <Input id='email' name='email' autoFocus />
-            </FormControl>
-            <FormControl margin='normal' required fullWidth>
-              <InputLabel htmlFor='password'>{password}</InputLabel>
-              <Input id='password' name='password' autoFocus />
-            </FormControl>
-            <FormControlLabel
-              control={<Checkbox color='primary' />}
-              label={remember}
-            />
-            <Button
-              variant='contained'
-              type='submit'
-              fullWidth
-              color='primary'
-              className={classes.submit}
-            >
-              {signIn}
-            </Button>
-          </form>
-        </Paper>
-      </main>
-    );
-  }
+function Form(props){
+  const { classes } = props;
+  const {language, toggleLanguage} = useContext(LanguageContext);
+  const { email, signIn, password, remember } = words[language];
+
+  return (
+    <main className={classes.main}>
+      <Paper className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant='h5'>{signIn}</Typography>
+        <Select onChange={toggleLanguage} value={language}>
+          <MenuItem value='english'>English</MenuItem>
+          <MenuItem value='french'>French</MenuItem>
+          <MenuItem value='spanish'>Spanish</MenuItem>
+        </Select>
+        <form className={classes.form}>
+          <FormControl margin='normal' required fullWidth>
+            <InputLabel htmlFor='email'>{email}</InputLabel>
+            <Input id='email' name='email' autoFocus />
+          </FormControl>
+          <FormControl margin='normal' required fullWidth>
+            <InputLabel htmlFor='password'>{password}</InputLabel>
+            <Input id='password' name='password' autoFocus />
+          </FormControl>
+          <FormControlLabel
+            control={<Checkbox color='primary' />}
+            label={remember}
+          />
+          <Button
+            variant='contained'
+            type='submit'
+            fullWidth
+            color='primary'
+            className={classes.submit}
+          >
+            {signIn}
+          </Button>
+        </form>
+      </Paper>
+    </main>
+  );
 }
+
 export default withStyles(styles)(Form);
